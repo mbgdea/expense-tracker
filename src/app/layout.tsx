4c88fd32e2e1d66230a9fde1dca087ee303d2fd0
Madalin Bogdea <madalinbogdea@icloud.com>
2025-01-03 22:23:35 +0200

    add Navbar component to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "Expense Tracker",
@@ -15,7 +16,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          <Navbar />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
