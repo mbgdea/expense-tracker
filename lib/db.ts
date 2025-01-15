4cf164cd4a50ac080f903f50e44ac1c1545bb66d
Madalin Bogdea <madalinbogdea@icloud.com>
2025-01-15 21:18:46 +0200

    add database connection setup with PrismaClient

---
+++ lib/db.ts
@@ -0,0 +1,13 @@
+/* eslint no-var: "off" */
+
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = db;
+}
