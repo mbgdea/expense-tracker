0d627b85f479d2da55253dd485dc332ce12d486b
Madalin Bogdea <madalinbogdea@icloud.com>
2025-01-16 23:42:52 +0200

    update Home component to display Guest or Header based on user authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
-const Home = () => {
-  return <h1>Expense Tracker</h1>;
+import Guest from "@/components/Guest";
+import { checkUser } from "@/lib/checkUser";
+import Header from "@/components/Header";
+
+const Home = async () => {
+  const user = await checkUser();
+
+  if (!user) {
+    return <Guest />;
+  }
+
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 
 export default Home;
