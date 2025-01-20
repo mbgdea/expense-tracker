bb1e9582b55631acda245188a8500e1a42c7cedd
Madalin Bogdea <madalinbogdea@icloud.com>
2025-01-20 14:48:58 +0200

    add Balance component to display user balance

---
+++ components/Balance.tsx
@@ -0,0 +1,14 @@
+import getUserBalance from "@/app/actions/getUserBalance";
+
+const Balance = async () => {
+  const { balance } = await getUserBalance();
+
+  return (
+    <>
+      <h4>Your Balance</h4>
+      <h1>${balance ?? 0}</h1>
+    </>
+  );
+};
+
+export default Balance;
