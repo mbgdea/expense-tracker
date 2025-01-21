1e4b1d3dcc43ff086fe76548d02b91739372d3bd
Madalin Bogdea <madalinbogdea@icloud.com>
2025-01-21 15:28:48 +0200

    add IncomeExpense component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Guest from "@/components/Guest";
 import { checkUser } from "@/lib/checkUser";
 import Header from "@/components/Header";
 import Balance from "@/components/Balance";
+import IncomeExpense from "@/components/IncomeExpense";
 import AddTransaction from "@/components/AddTransaction";
 
 const Home = async () => {
@@ -15,6 +16,7 @@ const Home = async () => {
     <>
       <Header />
       <Balance />
+      <IncomeExpense />
       <AddTransaction />
     </>
   );
