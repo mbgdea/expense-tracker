8670a32827387b67ea1fb91be5d13c98d5405925
Madalin Bogdea <madalinbogdea@icloud.com>
2025-01-22 20:41:16 +0200

    add TransactionList component to Home page for displaying transaction history

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import Balance from "@/components/Balance";
 import IncomeExpense from "@/components/IncomeExpense";
 import AddTransaction from "@/components/AddTransaction";
+import TransactionList from "@/components/TransactionList";
 
 const Home = async () => {
   const user = await checkUser();
@@ -18,6 +19,7 @@ const Home = async () => {
       <Balance />
       <IncomeExpense />
       <AddTransaction />
+      <TransactionList />
     </>
   );
 };
