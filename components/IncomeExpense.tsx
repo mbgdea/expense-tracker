290300fb289a782f37e1de4572dea960aac84ca9
Madalin Bogdea <madalinbogdea@icloud.com>
2025-01-25 23:06:42 +0200

    refactor IncomeExpense component layout and enhance styling for income and expense display

--- components/IncomeExpense.tsx
+++ components/IncomeExpense.tsx
@@ -1,21 +1,29 @@
 import getIncomeExpense from "@/app/actions/getIncomeExpense";
 import { addCommas } from "@/lib/utils";
+import { Card } from "./ui/card";
+import { Separator } from "./ui/separator";
 
 const IncomeExpense = async () => {
   const { income, expense } = await getIncomeExpense();
 
   return (
-    <div>
+    <Card className="mt-4 flex items-center justify-around p-2 text-center">
       <div>
-        <h4>Income</h4>
-        <p>${addCommas(Number(income?.toFixed(2)))}</p>
+        <h4 className="font-medium uppercase">Income</h4>
+        <p className="font-semibold text-green-700">
+          ${addCommas(Number(income?.toFixed(2)))}
+        </p>
       </div>
 
+      <Separator orientation="vertical" className="h-[1cm]" />
+
       <div>
-        <h4>Expense</h4>
-        <p>${addCommas(Number(expense?.toFixed(2)))}</p>
+        <h4 className="font-medium uppercase">Expense</h4>
+        <p className="font-semibold text-red-600">
+          ${addCommas(Number(expense?.toFixed(2)))}
+        </p>
       </div>
-    </div>
+    </Card>
   );
 };
 
