d881cc0cda25766eb843d2486ee228fdd721c2ee
Madalin Bogdea <madalinbogdea@icloud.com>
2025-01-25 22:05:56 +0200

    add utility function for conditional class names using clsx and tailwind-merge

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,10 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export function addCommas(num: number): string {
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
