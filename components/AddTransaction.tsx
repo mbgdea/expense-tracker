308e7635935f25de67d8eed34d4c8d138e2d20db
Madalin Bogdea <madalinbogdea@icloud.com>
2025-01-27 17:58:06 +0200

    refactor AddTransaction component layout and integrate shadcn Button and Separator components

--- components/AddTransaction.tsx
+++ components/AddTransaction.tsx
@@ -2,6 +2,8 @@
 
 import addTransaction from "@/app/actions/addTransaction";
 import { useRef } from "react";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
 
 const AddTransaction = () => {
   const formRef = useRef<HTMLFormElement>(null);
@@ -18,36 +20,44 @@ const AddTransaction = () => {
   };
 
   return (
-    <>
-      <h3>Add Transaction</h3>
+    <div className="mt-8">
+      <h3 className="text-xl font-medium">Add transaction</h3>
 
-      <form ref={formRef} action={clientAction}>
+      <Separator className="mb-4 mt-2" />
+
+      <form ref={formRef} action={clientAction} className="mb-8">
         <div>
-          <label htmlFor="text">Text</label>
+          <label htmlFor="text" className="block">
+            Text
+          </label>
           <input
             type="text"
             id="text"
             name="text"
             placeholder="Enter text..."
+            className="mt-1 w-full rounded-md border border-gray-300 p-1 px-2 outline-none"
           />
         </div>
 
-        <div>
-          <label htmlFor="amount">
-            Amount <br /> (negative - expense, positive - income)
-            <input
-              type="number"
-              name="amount"
-              id="amount"
-              placeholder="Enter amount..."
-              step="0.01"
-            />
+        <div className="mt-3">
+          <label htmlFor="amount" className="block">
+            Amount
           </label>
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            placeholder="Enter amount..."
+            step="0.01"
+            className="mt-1 w-full rounded-md border border-gray-300 p-1 px-2 outline-none"
+          />
         </div>
 
-        <button>Add transaction</button>
+        <Button className="mt-3 w-full bg-blue-600 hover:bg-blue-700">
+          Add transaction
+        </Button>
       </form>
-    </>
+    </div>
   );
 };
 
