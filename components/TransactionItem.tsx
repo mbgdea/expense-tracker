dcbb8aaadcf2625c3a21784ed04dc20395b2e5e8
Madalin Bogdea <madalinbogdea@icloud.com>
2025-01-27 18:56:26 +0200

    refactor TransactionItem component to use Card for layout and improve delete button styling

--- components/TransactionItem.tsx
+++ components/TransactionItem.tsx
@@ -3,6 +3,7 @@
 import { Transaction } from "@/types/Transaction";
 import { addCommas } from "@/lib/utils";
 import deleteTransaction from "@/app/actions/deleteTransaction";
+import { Card } from "./ui/card";
 
 const TransactionItem = ({ transaction }: { transaction: Transaction }) => {
   const sign = transaction.amount < 0 ? "-" : "+";
@@ -26,14 +27,18 @@ const TransactionItem = ({ transaction }: { transaction: Transaction }) => {
   return (
     <ul>
       <li>
-        {transaction.text}
-        <span>
-          {" "}
-          {sign} ${addCommas(Math.abs(transaction.amount))}
-        </span>
+        <Card className="relative mb-2 flex justify-between overflow-hidden px-2 py-1">
+          <span>{transaction.text}</span>
+          <span className="mr-1">
+            ${sign}
+            {addCommas(Math.abs(transaction.amount))}
+          </span>
+          <button
+            onClick={() => handleDeleteTransaction(transaction.id)}
+            className={`absolute right-0 top-0 h-full w-[5px] rounded-lg ${transaction.amount < 0 ? "bg-red-600" : "bg-green-700"}`}
+          ></button>
+        </Card>
       </li>
-
-      <button onClick={() => handleDeleteTransaction(transaction.id)}>x</button>
     </ul>
   );
 };
