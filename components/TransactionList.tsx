a1446e2c465bccd6ec9018d1b233bb1d8e150948
Madalin Bogdea <madalinbogdea@icloud.com>
2025-01-27 18:56:41 +0200

    enhance TransactionList layout with improved heading styling and added separator

--- components/TransactionList.tsx
+++ components/TransactionList.tsx
@@ -1,6 +1,7 @@
 import getTransactions from "@/app/actions/getTransactions";
 import { Transaction } from "@/types/Transaction";
 import TransactionItem from "./TransactionItem";
+import { Separator } from "./ui/separator";
 
 const TransactionList = async () => {
   const { transactions, error } = await getTransactions();
@@ -11,7 +12,10 @@ const TransactionList = async () => {
 
   return (
     <>
-      <h3>History</h3>
+      <h3 className="text-xl font-medium">History</h3>
+
+      <Separator className="mb-4 mt-2" />
+
       {transactions &&
         transactions.map((transaction: Transaction) => (
           <TransactionItem key={transaction.id} transaction={transaction} />
