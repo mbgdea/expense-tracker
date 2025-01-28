0241a2c22748a52ba2b070b894996222166a2570
Madalin Bogdea <madalinbogdea@icloud.com>
2025-01-28 22:07:34 +0200

    enhance Guest component with SignUpButton and improved layout

--- components/Guest.tsx
+++ components/Guest.tsx
@@ -1,11 +1,26 @@
-import { SignInButton } from "@clerk/nextjs";
+import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
 
 const Guest = () => {
   return (
-    <>
-      <p>Log In</p>
-      <SignInButton />
-    </>
+    <div className="container mt-4 max-w-80 p-2 text-center">
+      <h1 className="text-xl">
+        Welcome to <span className="font-medium">Expense Tracker</span>
+      </h1>
+      <p className="my-2">
+        Keep track of your spending and take control of your finances
+      </p>
+
+      <div className="space-x-4">
+        <Button variant={"blue"}>
+          <SignInButton />
+        </Button>
+
+        <Button variant={"green"}>
+          <SignUpButton />
+        </Button>
+      </div>
+    </div>
   );
 };
 
